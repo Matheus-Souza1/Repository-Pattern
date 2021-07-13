f7f7b1cacb071d5751f5071d06a4a0dd44b9d6ab
Matheus <matheussouzaslv2@gmail.com>
2021-07-13 14:06:26 -0300

    Modelando Dominio

---
+++ Repository-Pattern.Domain/Entities/Employer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Repository_Pattern.Domain.Entities
+{
+    public class Employer
+    {
+        public Employer() { }
+        public Employer( string name, string email, string document)
+        {
+            Id = Guid.NewGuid();
+            Name = name;
+            Email = email;
+            Document = document;
+        }
+
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Document { get; set; }
+
+        public void Update(string name, string email, string document)
+        {
+            Name = name;
+            Email = email;
+            Document = document;
+        }
+
+    }
+
+}
---
+++ Repository-Pattern.Domain/Repositories/IEmployerRepository.cs
@@ -0,0 +1,18 @@
+﻿using Repository_Pattern.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository_Pattern.Domain.Repositories
+{
+   public interface IEmployerRepository
+    {
+        Task<IEnumerable<Employer>> GetAll();
+        Task<Employer> GetById(Guid id);
+        void Add(Employer employer);
+        void Update(Employer employer);
+        void Delete(Guid id);
+    }
+}
